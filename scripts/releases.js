f07f71ddecbab817b6a961240c0d9d0e146ec38b
tedgravlin <theodoregravlin@pm.me>
2023-09-24 01:16:15 -0400

    Use Javascript for the platform selector
    This combines all of the platform pages into one page that shows the right platform based on the hash in the URL

---
+++ scripts/releases.js
@@ -0,0 +1,58 @@
+const url_params = new URLSearchParams(window.location.search);
+
+// On page load, show the right platform
+window.addEventListener("load", (event) => {
+    let hash = window.location.hash.substring(1);
+    switchRelease(hash);  
+});
+
+
+// Switches platform
+function switchRelease(platform) {
+    let android_container = document.getElementById("android");
+    let ios_container = document.getElementById("ios");
+    let desktop_container = document.getElementById("desktop");
+    let android_button = document.getElementById("android-button");
+    let ios_button = document.getElementById("ios-button");
+    let desktop_button = document.getElementById("desktop-button");
+
+    // Android
+    if (platform == 'android' || platform == "") {
+        // Containers
+        android_container.style.display = "block";
+        ios_container.style.display = "none";
+        desktop_container.style.display = "none";
+        // Buttons
+        android_button.className = "platform-selected";
+        ios_button.className = "platform-unselected";
+        desktop_button.className = "platform-unselected";
+        // URL
+        window.location.hash = "android";
+    }
+    // iOS
+    if (platform == 'ios') {
+        // Containers
+        android_container.style.display = "none";
+        ios_container.style.display = "block";
+        desktop_container.style.display = "none";
+        // Buttons
+        android_button.className = "platform-unselected";
+        ios_button.className = "platform-selected";
+        desktop_button.className = "platform-unselected";
+        // URL
+        window.location.hash = "ios";
+    }
+    // Desktop
+    if (platform == 'desktop') {
+        // Containers
+        android_container.style.display = "none";
+        ios_container.style.display = "none";
+        desktop_container.style.display = "block";
+        // Buttons
+        android_button.className = "platform-unselected";
+        ios_button.className = "platform-unselected";
+        desktop_button.className = "platform-selected";
+        // URL
+        window.location.hash = "desktop";
+    }
+}
