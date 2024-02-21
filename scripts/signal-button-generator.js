9afaff61d9461ae7047445c6403d0c2d36212a61
tedgravlin <theodoregravlin@pm.me>
2024-02-20 22:22:22 -0500

    Button Generator: Implement basic functionality

--- scripts/signal-button-generator.js
+++ scripts/signal-button-generator.js
@@ -11,9 +11,48 @@ window.addEventListener("load", (event) => {
     }
 });
 
+// Handle 'Generate' button press
+function handleClick() {
+    // Get the type from URL
+    let type = window.location.hash.substring(1);
+
+    // Username input
+    let username_link = document.getElementById('username-link').value;
+
+    // Phone Number input
+    let phone_number = document.getElementById('phone-number').value;
+    let phone_intl_code = document.getElementById('phone-intl-code').value;
+
+    // Group link input
+    let group_link = document.getElementById('group-link').value;
+
+    if (type == 'username') {
+        generateCode(username_link);
+    }
+    else if (type == 'phone') {
+        generateCode('https://signal.me/#p/' + '+' + phone_intl_code + phone_number);
+    }
+    else if (type == 'group') {
+        generateCode(group_link);
+    }
+
+}
+
+
 // Generate the HTML code and display it
-function generateHTML() {
-    let code_output = document.getElementById("code-output");
+function generateCode(link) {
+    let button_code = document.getElementById("button-code");
+    let button_preview = document.getElementById("button-preview");
+
+    let code = "<a target='_blank' href='" + link + "' style='font-family:Arial, Helvetica, sans-serif;" 
+    + "background-color: #3c76ee; color: white; padding: 0.5em 1em 0.5em 1em; border-radius: 24px;" 
+    + "display: grid;width: fit-content;grid-template-columns: 0.25fr 1fr;justify-items: center;" 
+    + "align-items: center;text-decoration: none;column-gap:5px;'><img style='width: 20px;" 
+    + "filter: brightness(0) saturate(100%) invert(100%) sepia(1%) saturate(7497%) hue-rotate(138deg) brightness(105%) contrast(101%);' " 
+    + "src='/assets/images/icons/signal-icon.png'><p style='margin: 0;font-size: 14px;'>Chat on Signal</p></a>";
+
+    button_code.innerText = code;
+    button_preview.innerHTML = code;
 }
 
 // Switched the generator type that's displayed based on button click
